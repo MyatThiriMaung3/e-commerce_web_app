42db96e1ec906d66f21b4fee0e4fd34d5d76d228
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-04-25 14:04:43 +0700

    Create gateway-service

---
+++ gateway-service/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get('/login', (req, res) => {
+    res.render('login', {error: null})
+});
+
+app.get('/logout', (req, res) => {
+    res.render('login', {error: "logout is pressed"})
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup', {error: null})
+});
+
+app.get('/error', (req, res) => {
+    res.render('error', {status: 404, title: "404 Error Occured", message: "Page not found"})
+});
+
+app.listen(3000);
+console.log('Gateway service is running on port 3000');
--- product-service/server.js
+++ product-service/server.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
-console.log('Product service is running on port 3000');
+app.listen(3002);
+console.log('Product service is running on port 3002');
