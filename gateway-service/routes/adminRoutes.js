d74ec700506cadd6dff1418904212cd15c0c7f77
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-05-09 02:40:21 +0700

    Add product update and variant management UI and functionalities

--- gateway-service/routes/adminRoutes.js
+++ gateway-service/routes/adminRoutes.js
@@ -101,6 +101,170 @@ router.post('/product-create',
   }
 );
 
+router.post('/product-update/:id',
+  upload.fields([
+    { name: 'productImage', maxCount: 1 },
+    { name: 'variantImages', maxCount: 10 }
+  ]),
+  async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const { name, description, brand, category } = req.body;
+
+      let product;
+
+      if (!req.files['productImage']) {
+        product = {
+          name: name,
+          description,
+          brand,
+          tag: category
+        };
+      } else {
+        const productImagePath = req.files['productImage'][0].filename;
+
+        product = {
+          name: name,
+          description,
+          brand,
+          tag: category,
+          image: productImagePath
+        };
+      }
+      
+      // Post to API
+      const response = await axios.put(`http://localhost:3002/api/products/${productId}`, product, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      res.redirect('/admin/product-details/' + productId);
+    } catch (err) {
+      console.error(err);
+      res.render('admin/product-create', {
+        error: 'Failed to create product',
+        title: 'Le administrateur - Create Product'
+      });
+    }
+  }
+);
+
+router.post('/variant-create/:productId',
+  upload.fields([
+    { name: 'variantImages', maxCount: 10 }
+  ]),
+  async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const { variantName, extraDescription, variantPrice, variantStock } = req.body;
+
+      const variantImages = req.files['variantImages'] || [];
+
+      const variant = {
+        variantName,
+        extraDescription,
+        price: parseFloat(variantPrice),
+        stock: parseInt(variantStock),
+        images: variantImages.map(image => image.filename)
+      };
+
+      // Post to API
+      const response = await axios.post(`http://localhost:3002/api/products/${productId}/variants`, variant, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+      res.redirect(`/admin/product-details/${productId}`);
+
+    } catch (err) {
+      console.error(err);
+      res.render('admin/product-create', {
+        error: 'Failed to create product',
+        title: 'Le administrateur - Create Product'
+      });
+    }
+  }
+);
+
+router.post('/variant-update/:productId/:variantId',
+  upload.fields([
+    { name: 'variantImages', maxCount: 10 }
+  ]),
+  async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const variantId = req.params.variantId;
+
+      const variantName = req.body[`variantName${variantId}`];
+      const extraDescription = req.body[`extraDescription${variantId}`];
+      const price = req.body[`variantPrice${variantId}`];
+      const stock = req.body[`variantStock${variantId}`];
+
+      let variant;
+
+      if (!req.files['variantImages' + variantId]) {
+        variant = {
+          variantName,
+          extraDescription,
+          price: parseFloat(price),
+          stock: parseInt(stock),
+        };
+      } else {
+
+        const variantImages = req.files[`variantImages${variantId}`] || [];
+
+        variant = {
+          variantName,
+          extraDescription,
+          price: parseFloat(price),
+          stock: parseInt(stock),
+          images: variantImages.map(image => image.filename)
+        };
+      }
+
+      // Post to API
+      const response = await axios.put(`http://localhost:3002/api/products/${productId}/variants/${variantId}`, variant, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+      res.redirect(`/admin/product-details/${productId}`);
+
+    } catch (err) {
+      console.error(err);
+      res.render('admin/product-create', {
+        error: 'Failed to create product',
+        title: 'Le administrateur - Create Product'
+      });
+    }
+  }
+);
+
+router.post('/variant-delete/:productId/:variantId', async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const variantId = req.params.variantId;
+
+    // Post to API
+    const response = await axios.delete(`http://localhost:3002/api/products/${productId}/variants/${variantId}`, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+    res.redirect(`/admin/product-details/${productId}`);
+
+  } catch (err) {
+    console.error(err);
+    res.render('admin/product-create', {
+      error: 'Failed to create product',
+      title: 'Le administrateur - Create Product'
+    });
+  }
+});
+
 router.get('/order-specific-details', adminController.renderAdminOrderSpecificDetails);
 
 module.exports = router;
--- product-service/routes/productRoutes.js
+++ product-service/routes/productRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const variantController = require('../controllers/variantController');
 
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
+
+router.post('/:productId/variants', variantController.addVariant);
+router.put('/:productId/variants/:variantId', variantController.updateVariant);
+router.delete('/:productId/variants/:variantId', variantController.deleteVariant);
 
 module.exports = router;
